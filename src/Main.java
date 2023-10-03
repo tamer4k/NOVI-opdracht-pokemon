bde37cc1c14d5455bee20b9cab2ac86f00d3b9dd
tamer <talashraf@snelstart.nl>
2023-10-03 14:44:29 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        BasePokemon pokemon1 = new ElectricPokemon("Pikachu ","Electric",300,4,3,"Groundblablabla");
+        BasePokemon pokemon1 = new ElectricPokemon("Pikachu ","Electric",300,4,3,"Ground6");
         BasePokemon pokemon2 = new FirePokemon("Charmander ","Fire",300,4,3,"Water");
         BasePokemon pokemon3 = new WaterPokemon("Squirtle  ","Water",300,3,4,"Electric");
         BasePokemon pokemon4 = new GrassPokemon("Bulbasaur ","Grass",300,3,3,"Fire");
